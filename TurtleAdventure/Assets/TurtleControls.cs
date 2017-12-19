5b9f7f0ad0ee84beac21784dfe337e3f1bd86f5f
Andrew_SIKK_09 <andreassilva@Andreass-MacBook-Air-3.local>
2017-12-19 11:00:59 +0100

    TurtleControls
    Första testet som funkar. kan nu gå höger och vänter samt hoppa.

---
+++ TurtleAdventure/Assets/TurtleControls.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurtleControls : MonoBehaviour {
+
+	public float SpeedX;
+	public float JumpForce;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		if ( Input.GetKey ("d") || Input.GetKey ("right")) {
+			transform.position += new Vector3 (SpeedX * Time.deltaTime, 0, 0);
+		}
+
+	if	( Input.GetKey ("a") || Input.GetKey ("left")) {
+			transform.position += new Vector3 (-SpeedX * Time.deltaTime,0,0);
+		}
+
+		if (Input.GetKey ("space")) {
+			GetComponent <Rigidbody2D> ().AddForce (new Vector2 (0, JumpForce));
+		}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	}
+}
