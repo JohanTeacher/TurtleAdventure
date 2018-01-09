db7dcb86ceeafa1b0379fb42694cb57dd5c08584
Andrew_SIKK_09 <andreassilva@Andreass-MacBook-Air-3.local>
2018-01-09 12:33:58 +0100

    hopp
    jobbade med hopp-kommandon

--- TurtleAdventure/Assets/TurtleControls.cs
+++ TurtleAdventure/Assets/TurtleControls.cs
@@ -6,9 +6,11 @@ public class TurtleControls : MonoBehaviour {
 
 	public float SpeedX;
 	public float JumpForce;
-
+	public int JumpCount;
 	// Use this for initialization
 	void Start () {
+
+		JumpCount = 0;
 		
 	}
 	
@@ -20,11 +22,12 @@ public class TurtleControls : MonoBehaviour {
 			transform.position += new Vector3 (SpeedX * Time.deltaTime, 0, 0);
 		}
 
-	if	( Input.GetKey ("a") || Input.GetKey ("left")) {
+	    if	( Input.GetKey ("a") || Input.GetKey ("left")) {
 			transform.position += new Vector3 (-SpeedX * Time.deltaTime,0,0);
 		}
 
-		if (Input.GetKey ("space")) {
+		if (Input.GetKeyDown ("space")) {
+			JumpCount++;
 			GetComponent <Rigidbody2D> ().AddForce (new Vector2 (0, JumpForce));
 		}
 
