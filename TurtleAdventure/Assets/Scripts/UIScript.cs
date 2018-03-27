13ffe8db05c58c4444a21838b895a0e11c8dd35b
Johan <johan.liewendahl@fryshuset.se>
2018-03-27 13:24:27 +0200

    UI
    Grodor i marginalen som minskar om man blir skadad mm.

---
+++ TurtleAdventure/Assets/Scripts/UIScript.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIScript : MonoBehaviour {
+
+	public GameObject lifeSignFirst;
+	public float spaceBetweenLifeSigns;
+
+	public List<GameObject> lifeSigns;
+
+	// Use this for initialization
+	void Start () {
+
+		lifeSigns = new List<GameObject> ();
+
+		lifeSignFirst.SetActive (false);
+
+	}
+
+	public void SetLifeSigns(int number)
+	{
+		for (int i = 0; i < number; i++) {
+			AddALifeSign ();
+		}
+	}
+
+	public void DeleteALifeSign()
+	{
+		GameObject lastLifeSign = lifeSigns [lifeSigns.Count - 1];
+		lifeSigns.Remove(lastLifeSign);
+		Destroy(lastLifeSign);
+	}
+
+	public void AddALifeSign()
+	{
+
+		//Instatiate the lifeSignPlaceholder
+		GameObject NewLS = Instantiate (lifeSignFirst,transform);
+
+		//Get the new lifeSign's transform
+		RectTransform NewLSTransform = NewLS.GetComponent<RectTransform> ();
+
+		//Set it to active
+		NewLS.SetActive (true);
+
+
+		//Move it to correct location according to list size and space between them etc.
+		NewLSTransform.localPosition = lifeSignFirst.transform.localPosition;
+		NewLSTransform.localPosition += new Vector3 ((float)lifeSigns.Count * spaceBetweenLifeSigns,0,0);
+
+		//Place it in list
+		lifeSigns.Add(NewLS);
+
+	}
+
+
+
+
+
+
+
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
